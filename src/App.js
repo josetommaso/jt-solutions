bc6807d704a37d4f588ee3a87282093914a729f0
Jose Tommaso <tommaso_j@hotmail.com>
2022-05-12 16:52:27 +0100

    new pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 
 function App() {
   return (
@@ -23,15 +26,15 @@ function App() {
         />
 
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/">
+            <Route index element={<Login />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="clients">
+              <Route index element={<List />} />
+              <Route path=":clientId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
         </Routes>
       </div>
     </AuthContextProvider>
